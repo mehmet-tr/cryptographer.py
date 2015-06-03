e1f5a50863470025d1cc6bde3cfd29d445d5b308
Caleb Cooper <caleb@caleb-cooper.net>
2015-06-03 12:42:09 -0400

    Completed compliant with flake8.

--- cryptographer.py
+++ cryptographer.py
@@ -12,7 +12,8 @@ Description: This program performs a two phase cryptographic function
              of the rounds are complete, the encrypted/decrypted message
              can either be printed to standard out or written to a file.
 Usage:
-cryptographer.py (-e|-d) -p PASSWORD -k KEYLENGTH (-m MESSAGE | -i INPUTFILE) [-o OUTPUTFILE] [-v | -vv]
+cryptographer.py (-e|-d) -p PASSWORD -k KEYLENGTH (-m MESSAGE | -i INPUTFILE) \
+[-o OUTPUTFILE] [-v | -vv]
 """
 
 import os
@@ -21,37 +22,46 @@ import argparse
 
 parser = argparse.ArgumentParser()
 action = parser.add_mutually_exclusive_group(required=True)
-action.add_argument('-e', '--encrypt', help='For encrypting a file/message.', action='store_true')
-action.add_argument('-d', '--decrypt', help='For decrypting a file/message.', action='store_true')
-parser.add_argument('-p', '--password', required=True, help='The passphase with which to encrypt/\
-    decrypt. If more than a word, use quotes.')
-parser.add_argument('-k', '--key', required=True, help='Determine key length. Suggested values\
-    between 10 and 1000, very high key lengths will take a long time.')
+action.add_argument('-e', '--encrypt', help='For encrypting a file/message.',
+                    action='store_true')
+action.add_argument('-d', '--decrypt', help='For decrypting a file/message.',
+                    action='store_true')
+parser.add_argument('-p', '--password', required=True, help='The passphase\
+                    with which to encrypt/decrypt. If more than a word, \
+                    use quotes.')
+parser.add_argument('-k', '--key', required=True, help='Determine key length.\
+                    Suggested values between 10 and 1000, very high key \
+                    lengths will take a long time.')
 message = parser.add_mutually_exclusive_group(required=True)
-message.add_argument('-m', '--message', help='The message to be encrypted/decrypted. Messages\
-    must be inside quotation marks.')
-message.add_argument('-i', '--inputfile', help='The file to be encrypted/decrypted.')
-parser.add_argument('-o', '--outputfile', help='The file in which to save the encrypted/decrypted\
-    message. If none is given, message will be printed to screen.')
-parser.add_argument('-v', '--verbose', help='-v will print out the progress of the encryption as\
-    a percentage. -vv will print the progress as well as the password, hashed password, and the\
-    message at every stage of the encryption/decryption process.', action='count')
+message.add_argument('-m', '--message', help='The message to be \
+                     encrypted/decrypted. Messages must be inside \
+                     quotation marks.')
+message.add_argument('-i', '--inputfile', help='The file to be\
+                     encrypted/decrypted.')
+parser.add_argument('-o', '--outputfile', help='The file in which to save the\
+                    encrypted/decrypted message. If none is given, message \
+                    will be printed to screen.')
+parser.add_argument('-v', '--verbose', help='-v will print out the progress \
+                    of the encryption as a percentage. -vv will print the \
+                    progress as well as the password, hashed password, and \
+                    the message at every stage of the encryption/decryption \
+                    process.', action='count')
 args = parser.parse_args()
 
 
 def variables(args):
-    """ Defines variables based on command line arguments. Also does some input checking on
-    key length variable to ensure it is a integer and greater than zero."""
-    
-    if  args.encrypt:
+    """ Defines variables based on command line arguments. Also does some
+    input checking on key length variable to ensure it is a integer and
+    greater than zero."""
+    if args.encrypt:
         function = "encrypt"
     elif args.decrypt:
         function = "decrypt"
     else:
-        print("You must specify if encryption (-e) or decryption (-d) should be used.")
+        print("You must specify if encryption (-e) or decryption (-d) should\
+        be used.")
         exit(1)
     password = args.password
-    
     try:
         keylength = int(args.key)
     except ValueError:
@@ -60,7 +70,6 @@ def variables(args):
     if keylength < 1:
         print("The key length must be greater than 0")
         exit(1)
-    
     if args.inputfile:
         if os.path.isfile(args.inputfile):
             in_file = open(args.inputfile)
@@ -70,25 +79,23 @@ def variables(args):
             exit(1)
     elif args.message:
         message = args.message
-        
     output_file = args.outputfile
-    
     if args.verbose:
         verbose = args.verbose
     else:
         verbose = 0
-    
     return function, message, output_file, verbose, password, keylength
 
 
 def phase1_crypto(password, nonce, rnum, message, function, verbose):
-    """ Phase 1 encrypts every character in the message by shifting it through the UTF-8
-    alphabet by a number derived from the character of the hashed password for the current
-    round and the nonce."""
+    """ Phase 1 encrypts every character in the message by shifting it through
+    the UTF-8 alphabet by a number derived from the character of the hashed
+    password for the current round and the nonce."""
     count = 1
     encrypted_message = ""
     for i in message:
-        offset = int(ord(password[count % (password.count('') - 1)])) * ord(nonce)
+        offset = int(ord(password[count % (password.count('') - 1)])) * \
+                 ord(nonce)
         if function == "encrypt":
             encrypted_char = chr(int(ord(i) + offset) % 55000)
         elif function == "decrypt":
@@ -99,12 +106,14 @@ def phase1_crypto(password, nonce, rnum, message, function, verbose):
     if verbose == 2:
         print("Round " + str(rnum) + "-- Phase 1: " + message)
     return message
-    
+
+
 def phase2_crypto(password, nonce, rnum, message, char, function, verbose):
-    """ Phase 2 encrypts every fifth character in the message, starting with the one in
-    the position of the round number modulus 5, by shifting it by a number derived from
-    the round number, nonce, and the ordinal position of the current round's character
-    from the hashed password devided by the length of the password."""
+    """ Phase 2 encrypts every fifth character in the message, starting with
+    the one in the position of the round number modulus 5, by shifting it by
+    a number derived from the round number, nonce, and the ordinal position of
+    the current round's character from the hashed password devided by the
+    length of the password."""
     count = 1
     rnonce = rnum * ord(nonce)
     encrypted_message = ""
@@ -124,10 +133,11 @@ def phase2_crypto(password, nonce, rnum, message, char, function, verbose):
         print("Round " + str(rnum) + "-- Phase 2: " + message)
     return message
 
+
 def hash_pass(password, keylength, verbose):
-    """ The password is hashed to ensure that the resulting hashed password will meet
-    the keylength requirements given by the user. This allows the user to have a
-    secure password without having to remember it."""
+    """ The password is hashed to ensure that the resulting hashed password
+    will meet the keylength requirements given by the user. This allows the
+    user to have a secure password without having to remember it."""
     if verbose == 2:
         print("Unhashed password: " + password)
     t1 = len(password) + 2
@@ -144,11 +154,13 @@ def hash_pass(password, keylength, verbose):
     if verbose == 2:
         print("Hashed password: " + password)
 
+
 def main(args):
     """Performs all of the nessacerry setup and clean up to encrypt or
     decrypt a message based on the -e or -d arugments.
     Also handles writing to the output file or standard out."""
-    function, message, output_file, verbose, password, keylength = variables(args)
+    function, message, output_file, verbose, password, keylength = \
+        variables(args)
     if function == "encrypt":
         nonce = chr(int(time.time() * 10000000) % 55000)
     elif function == "decrypt":
@@ -159,8 +171,10 @@ def main(args):
     if message:
         rnum = 1
         for char in password:
-            message = phase1_crypto(password, nonce, rnum, message, function, verbose)
-            message = phase2_crypto(password, nonce, rnum, message, char, function, verbose)
+            message = phase1_crypto(password, nonce, rnum, message, function,
+                                    verbose)
+            message = phase2_crypto(password, nonce, rnum, message, char,
+                                    function, verbose)
             if verbose > 0:
                 print((rnum / len(password)) * 100, "% Complete.")
             rnum += 1
@@ -170,7 +184,7 @@ def main(args):
             operation = "En"
         elif function == "decrypt":
             operation = "De"
-            
+
         if output_file:
             out_file = open(output_file, 'w')
             out_file.write(message)
@@ -183,7 +197,7 @@ def main(args):
         if verbose > 0:
             print(operation+"cryption complete.")
     else:
-        print("You must either enter a message (-m) or specify a input file (-i).")
+        print("Enter a message (-m) or specify a input file (-i).")
         exit(1)
 
 main(args)
