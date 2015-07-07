897f816dd053f2e38cbdf955280beafaf4e84e7c
Caleb Cooper <coopercd@ornl.gov>
2015-07-07 11:54:30 -0400

    Added more features to test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-python3 cryptographer.py -e -p pass -k 10 -i test -o test.encrypted
-test $(md5sum test| cut -f 1 -d ' ') != $(md5sum test.encrypted | cut -f 1 -d ' ')
-python3 cryptographer.py -d -p pass -k 10 -i test.encrypted -o test.decrypted
-test $(md5sum test| cut -f 1 -d ' ') = $(md5sum test.decrypted | cut -f 1 -d ' ')
+python3 cryptographer.py -e -p pass -k 10 -i test -o test.encrypted || exit 1
+test $(md5sum test| cut -f 1 -d ' ') != $(md5sum test.encrypted | cut -f 1 -d ' ') || exit 1
+python3 cryptographer.py -d -p pass -k 10 -i test.encrypted -o test.decrypted || exit 1
+test $(md5sum test| cut -f 1 -d ' ') = $(md5sum test.decrypted | cut -f 1 -d ' ') || exit 1
+
+rm test.encrypted
+rm test.decrypted
+echo 'Build successful!'
